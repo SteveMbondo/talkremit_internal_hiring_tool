6ce899d1c6523b4b3bdc622ea8de01c90d98c543
root <+>
2025-11-16 23:23:00 +0300

    config edit

--- next.config.js
+++ next.config.js
@@ -1,13 +1,8 @@
-/** @type {import('next').NextConfig} */
+@type {import('next').NextConfig}
 const nextConfig = {
+
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'prod-refactor-cms.talkremit.com',
-        pathname: '/wp-content/uploads/**',
-      },
-    ],
+    domains: ['prod-refactor-cms.talkremit.com'],
   },
 };
 
