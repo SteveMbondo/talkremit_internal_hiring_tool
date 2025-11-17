8ef7d2437ab1e722c7b0d40c2d68dee08f296c29
SteveMbondo <96119603+SteveMbondo@users.noreply.github.com>
2025-11-17 09:43:07 +0300

    Adjust logo container for responsive design

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,13 +26,14 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             <section className="relative bg-blue-600 text-white py-20 overflow-hidden">
               <div className="absolute inset-0 flex">
                 <div className="flex-1 bg-gradient-to-br from-[#4737a4] to-[#36f8fc]">
-                  <div className="mt-4 ml-2">
-                    <Image 
-                    src="https://prod-refactor-cms.talkremit.com/wp-content/uploads/2021/05/TR-logo-light.svg"
-                    alt="TalkRemit Logo"
-                    width={173}
-                    height={28}
-                    />
+                  <div className="mt-4 ml-2 w-fit max-w-full">
+                    <Image
+                      src="https://prod-refactor-cms.talkremit.com/wp-content/uploads/2021/05/TR-logo-light.svg"
+                      alt="TalkRemit Logo"
+                      width={173}
+                      height={28}
+                      className="h-auto w-[140px] max-w-full"
+                     />
                   </div>
                 </div>
               </div>
